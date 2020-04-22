eeb4641b2de9fd63e8bbebbbdd6409c974c01f3a
wakye5815 <wakye5815@gmail.com>
2020-04-22 20:55:28 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true,
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+        "prettier/@typescript-eslint"
+    ],
+    "globals": {
+        "Atomics": 'readonly',
+        "SharedArrayBuffer": 'readonly',
+    },
+    "parser": '@typescript-eslint/parser',
+    "parserOptions": {
+        "ecmaVersion": 2019,
+        "sourceType": 'module',
+    },
+    "plugins": ['@typescript-eslint'],
+    "rules": {
+        "prettier/prettier": [
+            "error",
+            {
+                "semi": true,
+                "singleQuote": true,
+                "printWidth": 120,
+                "trailingComma": "all"
+            }
+        ],
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/prefer-interface": "off",
+        "@typescript-eslint/no-object-literal-type-assertion": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-parameter-properties":"off",
+        "linebreak-style": [
+            "error",
+            "unix"
+        ]
+    }
+};
